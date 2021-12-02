7c3af2d04c408ee89f237ec31e49e87a9cb141bf
Chris Vest <christianvest_hansen@apple.com>
2021-12-02 14:00:05 +0100

    Copy over the code from the main netty repo

--- src/main/java/io/netty/contrib/handler/pcap/package-info.java
+++ src/main/java/io/netty/contrib/handler/pcap/package-info.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2021 The Netty Project
+ * Copyright 2020 The Netty Project
  *
  * The Netty Project licenses this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at:
  *
- *   https://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -15,6 +15,6 @@
  */
 
 /**
- * Template contrib package-info.
+ * Capture data and write into Pcap format which helps in troubleshooting.
  */
-package io.netty.contrib.template;
+package io.netty.contrib.handler.pcap;
